1fa01756eeef92039c14b85961a5e2b58b5b6764
Abhishek Kumar <ckabhishek96.srm@gmail.com>
2026-04-11 20:56:36 +0530

    fix: generate public url for minio signed url

--- src/modules/files/minio.service.ts
+++ src/modules/files/minio.service.ts
@@ -6,11 +6,23 @@ import { MinIOConnectionError } from './exceptions/file-errors';
 @Injectable()
 export class MinioService implements OnModuleInit {
   private minioClient!: Minio.Client;
-  private minioSignedUrlClient!: Minio.Client;
   private readonly logger = new Logger(MinioService.name);
   private readonly defaultBucket = 'app-bucket-xtvnlqt';
 
   constructor(private readonly configService: ConfigService) {}
+
+  private getPublicUrl = (url: string): string => {
+    const endpoint = this.configService.get<string>(
+      'MINIO_ENDPOINT',
+      'localhost',
+    );
+    const port = this.configService.get<string>('MINIO_PORT', '9000');
+    const publicEndpoint = this.configService.get<string>(
+      'MINIO_SIGNED_URL_ENDPOINT',
+      'localhost:9000',
+    );
+    return url.replace(`http://${endpoint}:${port}`, publicEndpoint);
+  };
 
   async onModuleInit() {
     try {
@@ -34,31 +46,6 @@ export class MinioService implements OnModuleInit {
         await this.minioClient.makeBucket(this.defaultBucket, 'us-east-1');
       }
       this.logger.log('MinIO client initialized successfully');
-
-      // Minio client for signed URL generation must be initialized before checking bucket existence
-      // this will not get connected to minio server.
-      this.minioSignedUrlClient = new Minio.Client({
-        endPoint: this.configService.get<string>(
-          'MINIO_SIGNED_URL_ENDPOINT',
-          'localhost',
-        ),
-        port: parseInt(
-          this.configService.get<string>('MINIO_SIGNED_URL_PORT', '9000'),
-          10,
-        ),
-        useSSL:
-          this.configService.get<string>(
-            'MINIO_SIGNED_URL_USE_SSL',
-            'false',
-          ) === 'true',
-        accessKey: this.configService.get<string>('MINIO_ACCESS_KEY', 'admin'),
-        secretKey: this.configService.get<string>(
-          'MINIO_SECRET_KEY',
-          'password',
-        ),
-      });
-
-      this.logger.log('MinIO signed URL client initialized successfully');
     } catch (error) {
       this.logger.error('Failed to initialize MinIO client', error);
       throw new MinIOConnectionError('Could not connect to storage service');
@@ -75,18 +62,23 @@ export class MinioService implements OnModuleInit {
     expirySeconds: number,
   ): Promise<string> {
     try {
+      let url: string;
       if (method === 'GET') {
-        return await this.minioSignedUrlClient.presignedGetObject(
+        url = await this.minioClient.presignedGetObject(
           this.defaultBucket,
           objectKey,
           expirySeconds,
         );
+
+        return this.getPublicUrl(url);
       } else {
-        return await this.minioSignedUrlClient.presignedPutObject(
+        url = await this.minioClient.presignedPutObject(
           this.defaultBucket,
           objectKey,
           expirySeconds,
         );
+
+        return this.getPublicUrl(url);
       }
     } catch (error) {
       this.logger.error(
@@ -108,12 +100,13 @@ export class MinioService implements OnModuleInit {
         'response-content-type': 'application/octet-stream',
       };
 
-      return await this.minioSignedUrlClient.presignedGetObject(
+      const url: string = await this.minioClient.presignedGetObject(
         this.defaultBucket,
         objectKey,
         expirySeconds,
         requestParams,
       );
+      return this.getPublicUrl(url);
     } catch (error) {
       this.logger.error(
         `Failed to generate presigned download URL for ${objectKey}`,
@@ -199,14 +192,14 @@ export class MinioService implements OnModuleInit {
     expirySeconds: number = 86400,
   ): Promise<string> {
     try {
-      const url = await this.minioSignedUrlClient.presignedUrl(
+      const url = await this.minioClient.presignedUrl(
         'PUT',
         this.defaultBucket,
         objectKey,
         expirySeconds,
         { uploadId, partNumber: partNumber.toString() },
       );
-      return url;
+      return this.getPublicUrl(url);
     } catch (error) {
       this.logger.error(
         `Failed to generate presigned URL for part ${partNumber} of ${objectKey}`,
